cb2e2cae76caf4d023001249563cc087fca74826
Nabeeha Mudassir <nabeeha.mudassir1@gmail.com>
2024-05-07 18:11:19 +0500

    firebase connected

--- app/src/main/java/com/example/spendsmart/Login.java
+++ app/src/main/java/com/example/spendsmart/Login.java
@@ -5,21 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    Button loginbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        init();
+
+
+    }
+
+    public void init()
+    {
         TextView signUpTextView = findViewById(R.id.signUpTextView);
+        loginbtn = findViewById(R.id.loginbtn);
+
+
         signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Redirect user to sign-up screen
                 Intent intent = new Intent(Login.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, DashboardActivity.class);
                 startActivity(intent);
             }
         });
