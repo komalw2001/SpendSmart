1ec56736677a790acbee98090a77c2a8f8fa303a
komalw2001 <komalwaseemm@gmail.com>
2024-05-11 23:07:04 +0500

    overview started

--- app/src/main/java/com/example/spendsmart/OverviewFragment.java
+++ app/src/main/java/com/example/spendsmart/OverviewFragment.java
@@ -1,18 +1,33 @@
 package com.example.spendsmart;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class OverviewFragment extends Fragment
 {
     Context context;
+
+    TextView tvBalance;
+
+    DatabaseReference dbRef;
 
     public OverviewFragment(Context c) {
         context = c;
@@ -28,5 +43,35 @@ public class OverviewFragment extends Fragment
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
+        SharedPreferences sp = context.getSharedPreferences("user_info", Context.MODE_PRIVATE);
+        String user = sp.getString("session_user",null);
+
+        tvBalance = view.findViewById(R.id.tvBalance);
+
+        dbRef.child("Users")
+                .child(user)
+                .child("balance")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()) {
+                            Double balance = snapshot.getValue(Double.class);
+                            tvBalance.setText(balance.toString());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.d("Showing Cash","Error: "+error.getMessage());
+                    }
+                });
     }
 }
